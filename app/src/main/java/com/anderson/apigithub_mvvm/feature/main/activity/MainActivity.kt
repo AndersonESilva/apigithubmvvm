8ccdb5ed9ff340fb993ed62e5345647e08267901
AndersonESilva <andersoneraldodasilva@live.com>
2019-09-21 19:03:42 -0300

    list adapter

--- app/src/main/java/com/anderson/apigithub_mvvm/feature/main/activity/MainActivity.kt
+++ app/src/main/java/com/anderson/apigithub_mvvm/feature/main/activity/MainActivity.kt
@@ -3,12 +3,15 @@ package com.anderson.apigithub_mvvm.feature.main.activity
 import br.com.anderson.apigithub_mvvm.ui.generic.base.activity.BaseActivity
 import com.anderson.apigithub_mvvm.R
 import com.anderson.apigithub_mvvm.databinding.ActivityMainBinding
+import com.anderson.apigithub_mvvm.feature.main.adapter.RepositoryAdapter
 import com.anderson.apigithub_mvvm.feature.main.viewmodel.MainViewModel
 
 /**
  * Created by anderson on 21/09/19.
  */
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
+
+    private lateinit var RepositoryAdapter: RepositoryAdapter
 
     override fun getLayoutId(): Int {
         return R.layout.activity_main
@@ -18,5 +21,7 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
 
     override fun init() {
         bind.viewModel = viewModel
+
+        bind.listView
     }
 }
