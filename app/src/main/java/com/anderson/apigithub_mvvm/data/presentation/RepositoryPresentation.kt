480d52fcbf48fc408a6aafa743b9399ce67e7c7b
AndersonESilva <andersoneraldodasilva@live.com>
2019-09-21 20:50:16 -0300

    Implementação da busca

--- app/src/main/java/com/anderson/apigithub_mvvm/data/presentation/RepositoryPresentation.kt
+++ app/src/main/java/com/anderson/apigithub_mvvm/data/presentation/RepositoryPresentation.kt
@@ -1,4 +1,9 @@
 package com.anderson.apigithub_mvvm.data.presentation
 
-class RepositoryPresentation {
-}
+class RepositoryPresentation (
+    val name: String,
+    val imgUrl: String,
+    val starsCount: String,
+    val forksCount: String,
+    val pullsUrls: String
+)
--- app/src/main/java/com/anderson/apigithub_mvvm/feature/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/anderson/apigithub_mvvm/feature/main/viewmodel/MainViewModel.kt
@@ -1,6 +1,11 @@
 package com.anderson.apigithub_mvvm.feature.main.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import br.com.anderson.apigithub_mvvm.ui.generic.base.viewmodel.BaseViewModel
+import com.anderson.apigithub_mvvm.data.presentation.RepositoryPresentation
 import com.anderson.apigithub_mvvm.service.MainRepository
 import javax.inject.Inject
 
@@ -8,4 +13,37 @@ import javax.inject.Inject
  * Created by anderson on 21/09/19.
  */
 class MainViewModel @Inject constructor(val repository: MainRepository) : BaseViewModel() {
+
+    val lisAux : MutableList<RepositoryPresentation> = arrayListOf()
+
+    var mMediatorRespositories = MediatorLiveData<List<RepositoryPresentation>>()
+    var mLiveDataRespositories = MutableLiveData<List<RepositoryPresentation>>()
+
+    fun getListRepository(page: Int){
+
+        mMediatorRespositories.addSource(getListRepositoryLiveDate(page)){
+            mLiveDataRespositories.value = it
+        }
+    }
+
+    fun getListRepositoryLiveDate(page: Int): LiveData<List<RepositoryPresentation>> {
+
+        val dataLiveData: LiveData<List<RepositoryPresentation>> = Transformations.map(repository.getListRepository(page)){
+
+            it.forEach { input ->
+                lisAux.add(
+                    RepositoryPresentation(
+                        input.name,
+                        input.imgUrl,
+                        input.starsCount.toString(),
+                        input.forksCount.toString(),
+                        input.pullsUrls
+                ))
+            }
+
+            lisAux
+        }
+
+        return dataLiveData
+    }
 }
