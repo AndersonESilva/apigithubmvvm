1255a52af38070b1bee87a9127e685c623be1901
AndersonESilva <andersoneraldodasilva@live.com>
2019-09-21 16:54:10 -0300

    Adapter implementado

--- app/src/main/java/com/anderson/apigithub_mvvm/feature/main/adapter/RepositoryAdapter.kt
+++ app/src/main/java/com/anderson/apigithub_mvvm/feature/main/adapter/RepositoryAdapter.kt
@@ -1,23 +1,41 @@
 package com.anderson.apigithub_mvvm.feature.main.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import androidx.databinding.DataBindingUtil
+import com.anderson.apigithub_mvvm.R
+import com.anderson.apigithub_mvvm.data.presentation.RepositoryPresentation
 
-class RepositoryAdapter constructor() : BaseAdapter() {
-    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+/**
+ * Created by anderson on 21/09/19.
+ */
+class RepositoryAdapter constructor(
+    var list: ArrayList<RepositoryPresentation>
+) : BaseAdapter() {
+
+    override fun getItem(position: Int): Any {
+        return list[position]
     }
 
-    override fun getItem(p0: Int): Any {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getItemId(p0: Int): Long {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
     }
 
     override fun getCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return list.size
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        //val binding: ItemMovieBinding
+        var view  = LayoutInflater.from(parent?.context).inflate(R.layout.item_repository, null)
+
+//        binding = DataBindingUtil.bind(view)!!
+//        binding.item = this.moviesList[position]
+//
+//        return  binding.root
+
+        return view
     }
 }
