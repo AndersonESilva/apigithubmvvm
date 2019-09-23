e8bfca450dbabd7b0c9053ada7ac779e601a1084
AndersonESilva <andersoneraldodasilva@live.com>
2019-09-22 21:31:56 -0300

    Implementado o adapter de pull request

--- app/src/main/java/com/anderson/apigithub_mvvm/feature/pullRequest/activity/PullRequestActivity.kt
+++ app/src/main/java/com/anderson/apigithub_mvvm/feature/pullRequest/activity/PullRequestActivity.kt
@@ -1,6 +1,7 @@
 package com.anderson.apigithub_mvvm.feature.pullRequest.activity
 
 import android.os.Build
+import android.view.View
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.Observer
 import br.com.anderson.apigithub_mvvm.ui.generic.base.activity.BaseActivity
@@ -33,10 +34,14 @@ class PullRequestActivity : BaseActivity<ActivityPullRequestBinding, PullRequest
     override fun init() {
         bind.viewModel = viewModel
 
+        repositoryPresentation = (getIntent().getExtras()?.getSerializable(REPO_OBJ) as? RepositoryPresentation)!!
+
         viewModel.getListRepositoryLiveDate(repositoryPresentation.login, repositoryPresentation.name).observe(this, Observer {
             pullRequestAdapter = PullRequestAdapter(it as ArrayList<PullRequestPresentation>)
 
             bind.listView.adapter = pullRequestAdapter
+            bind.progressBar.visibility = View.INVISIBLE
+
         })
     }
 
--- app/src/main/java/com/anderson/apigithub_mvvm/feature/pullRequest/viewmodel/PullRequestViewModel.kt
+++ app/src/main/java/com/anderson/apigithub_mvvm/feature/pullRequest/viewmodel/PullRequestViewModel.kt
@@ -1,14 +1,10 @@
 package com.anderson.apigithub_mvvm.feature.pullRequest.viewmodel
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
 import br.com.anderson.apigithub_mvvm.ui.generic.base.viewmodel.BaseViewModel
 import com.anderson.apigithub_mvvm.data.presentation.PullRequestPresentation
 import com.anderson.apigithub_mvvm.service.PullRequestRepository
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
 /**
@@ -18,7 +14,6 @@ class PullRequestViewModel @Inject constructor(val repository: PullRequestReposi
 
     val lisAux : MutableList<PullRequestPresentation> = arrayListOf()
 
-    @RequiresApi(Build.VERSION_CODES.O)
     fun getListRepositoryLiveDate(creator : String, repositoryName: String): LiveData<List<PullRequestPresentation>> {
 
         return Transformations.map(repository.getPullsRequest(creator, repositoryName)){
@@ -30,7 +25,7 @@ class PullRequestViewModel @Inject constructor(val repository: PullRequestReposi
                             input.user.login,
                             input.user.avatarUrl,
                             input.title,
-                            LocalDate.parse(input.updatedDate, DateTimeFormatter.ISO_DATE).toString(),
+                            formtDate(input.updatedDate),
                             input.body
                         )
                     )
@@ -43,5 +38,12 @@ class PullRequestViewModel @Inject constructor(val repository: PullRequestReposi
 
             lisAux
         }
+    }
+
+    private fun formtDate(dateString: String): String{
+
+        //LocalDate.parse(input.updatedDate, DateTimeFormatter.ISO_DATE).toString()
+
+        return dateString
     }
 }
