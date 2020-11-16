4d8272061870142a2e594ab77a67f7f0af921619
AndersonESilva <andersoneraldodasilva@live.com>
2020-11-16 00:35:51 -0300

    Implemented getPullRequestes

---
+++ app/src/main/java/com/anderson/apigithub_mvvm/feature/pullRequest/converter/PullRequestConverter.kt
@@ -0,0 +1,30 @@
+package com.anderson.apigithub_mvvm.feature.pullRequest.converter
+
+import com.anderson.apigithub_mvvm.data.presentation.PullRequestPresentation
+import com.anderson.apigithub_mvvm.data.response.PullRequestResponse
+import com.anderson.apigithub_mvvm.feature.common.BaseConverter
+import javax.inject.Inject
+
+/**
+ * Created by anderson on 16/11/2020.
+ */
+class PullRequestConverter @Inject constructor() : BaseConverter<List<PullRequestResponse>, List<PullRequestPresentation>> {
+
+    override fun convert(input: List<PullRequestResponse>): List<PullRequestPresentation> {
+        val data = mutableListOf<PullRequestPresentation>()
+        input.forEach { input ->
+            data.add(
+                PullRequestPresentation(
+                    input.user.login,
+                    input.user.avatarUrl,
+                    input.title,
+                    input.updatedDate,
+                    input.body,
+                    input.htmlUrl
+                )
+            )
+        }
+        return data
+    }
+
+}
--- app/src/main/java/com/anderson/apigithub_mvvm/feature/pullRequest/viewmodel/PullRequestViewModel.kt
+++ app/src/main/java/com/anderson/apigithub_mvvm/feature/pullRequest/viewmodel/PullRequestViewModel.kt
@@ -1,22 +1,58 @@
 package com.anderson.apigithub_mvvm.feature.pullRequest.viewmodel
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
-import com.anderson.apigithub_mvvm.feature.common.BaseViewModel
+import com.anderson.apigithub_mvvm.common.Coroutines
 import com.anderson.apigithub_mvvm.data.presentation.PullRequestPresentation
+import com.anderson.apigithub_mvvm.data.presentation.RepositoryPresentation
+import com.anderson.apigithub_mvvm.data.response.Resource
+import com.anderson.apigithub_mvvm.feature.common.BaseViewModel
+import com.anderson.apigithub_mvvm.feature.pullRequest.converter.PullRequestConverter
+import com.anderson.apigithub_mvvm.service.GitHubRepository
 import com.anderson.apigithub_mvvm.service.PullRequestRepository
+import kotlinx.coroutines.Job
 import javax.inject.Inject
 
 /**
  * Created by anderson on 22/09/19.
  */
-class PullRequestViewModel @Inject constructor(val repository: PullRequestRepository) : BaseViewModel(){
+class PullRequestViewModel @Inject constructor(private val repository: GitHubRepository,
+                                               private val converter: PullRequestConverter,
+                                               private val repositoryx: PullRequestRepository) : BaseViewModel(){
+
+
+    private val _resource = MutableLiveData<Resource<List<PullRequestPresentation>>>()
+    private lateinit var job: Job
+
+    val resource: LiveData<Resource<List<PullRequestPresentation>>>
+        get() = _resource
+
+    init {
+        _resource.value = Resource.init()
+    }
+
+    fun getPullRequestes(creator : String, repositoryName: String){
+        job = Coroutines.ioThenMain(
+            { repository.getPullRequestes(creator, repositoryName) }, {
+                if(it?.data != null){
+                    val list = mutableListOf<PullRequestPresentation>()
+                    _resource.value?.data?.let { it1 -> list.addAll(it1) }
+                    val listConverted = converter.convert(it.data)
+                    list.addAll(listConverted)
+                    _resource.value = Resource.success(list)
+                }else{
+                    _resource.value = it?.error?.let { it1 -> Resource.error(it1) }
+                }
+            }
+        )
+    }
 
     val lisAux : MutableList<PullRequestPresentation> = arrayListOf()
 
     fun getListRepositoryLiveDate(creator : String, repositoryName: String): LiveData<List<PullRequestPresentation>> {
 
-        return Transformations.map(repository.getPullsRequest(creator, repositoryName)){
+        return Transformations.map(repositoryx.getPullsRequest(creator, repositoryName)){
 
             if(it != null){
                 it.forEach { input ->
